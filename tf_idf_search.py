7ca40281c9adf341030bb8ca761a6607093b57db
lunno08 <baejune6155@gmail.com>
2026-01-16 15:52:32 +0900

    submit-1

--- tf_idf_search.py
+++ tf_idf_search.py
@@ -21,6 +21,10 @@ def compute_tf(document: str) -> dict:
 
     # Compute term frequency (TF)
     # Implement your code here
+    for word in tokens: tf[word] = tf.get(word, 0) + 1
+
+    word_total = len(tokens)
+    for word in tf: tf[word] /= word_total
 
     return tf
 
@@ -44,6 +48,11 @@ def compute_idf(docs: list[str]) -> dict:
     # Compute inverse document frequency (IDF), given entire vocabulary \
     # from all the documents (all_words)
     # Implement your code here
+    for word in all_words:
+        df = 0
+        for doc in docs:
+            if word in tokenize(doc): df+=1
+        idf[word] = math.log(N / df)
 
     return idf
 
@@ -63,6 +72,7 @@ def compute_tf_idf(document: str, idf: dict) -> dict:
 
     # Compute TF-IDF
     # Implement your code here
+    for word, tf_value in tf.items(): tf_idf[word] = tf_value * idf.get(word, 0)
 
     return tf_idf
 
